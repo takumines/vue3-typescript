455bd84987e6ab8a7072a9dd5b073f376a3ff388
takumi_kurogi <takumines1@gmail.com>
2022-05-18 22:49:29 +0900

    削除時に確認モーダルを表示するよう修正

--- src/components/PersonList.vue
+++ src/components/PersonList.vue
@@ -7,8 +7,10 @@ type Props = {
 
 const props = defineProps<Props>()
 const emit = defineEmits(['delete'])
-const onClickDelete = (id: number) => {
-  emit('delete', id)
+const onClickDelete = (id: number, name: string) => {
+  if (confirm('Delete ' + name + '?')) {
+    emit('delete', id)
+  }
 }
 </script>
 
@@ -16,7 +18,7 @@ const onClickDelete = (id: number) => {
   <li v-for="person in props.persons" :key="person.id" class="person-list">
     <span>{{ person.name }}</span>
     <span>{{ person.age }}</span>
-    <button @click="onClickDelete(person.id)">
+    <button @click="onClickDelete(person.id, person.name)">
       <span>delete</span>
     </button>
   </li>
